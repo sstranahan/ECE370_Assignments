e591dcd0379709a467c24a573d2b02ae819b53af
Stephen Stranahan <49105822+sstranahan@users.noreply.github.com>
2020-09-10 10:31:26 -0400

    Add files via upload
    Pretty much finished with the first assignment (sort and search implementation)

--- ECE370_Assignments/Assignment_1/Assignment_1/people.h
+++ ECE370_Assignments/Assignment_1/Assignment_1/people.h
@@ -10,15 +10,24 @@ using namespace std;
 #ifndef ASSIGNMENT_1_PEOPLE_H
 #define ASSIGNMENT_1_PEOPLE_H
 
+//////////////////////// Defines Person Struct ////////////////////////////////////
 struct person {
     string Last_Name;
     string First_Name;
     int ID;
     string Address;
 };
+///////////////////////////////////////////////////////////////////////////////////
 
+///////////////////////////////////////////////////////////////////////////////////
+// Function:       Prints a person record
+//
+// Parameters:     Person to be printed
+//
+// Return Value:   Void
+///////////////////////////////////////////////////////////////////////////////////
 void printPerson(person person) {
-    cout << person.Last_Name << "," << person.First_Name << endl;
+    cout << person.Last_Name << ", " << person.First_Name << endl;
     cout << person.ID << endl;
     cout << person.Address << endl;
     return;
